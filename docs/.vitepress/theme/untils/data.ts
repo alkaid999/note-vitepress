21bdd64f7d2ae6f76d668b0c622f02638298e0d6
alkaid <alkaid.yang@qq.com>
2025-07-08 10:57:17 +0800

    docs: 更新导航数据
    - 添加 JX3Box 到常用工具列表
    - 增加腾讯元宝和抖音豆包到在线 AI 工具列表

--- docs/.vitepress/theme/untils/data.ts
+++ docs/.vitepress/theme/untils/data.ts
@@ -14,7 +14,18 @@ export const NAV_DATA: NavData[] = [
         desc: '在线图片压缩工具',
         link: 'https://tinypng.com'
       },
+      {
+        icon: 'https://cdn.jx3box.com/static/index/favicon.ico',
+        title: 'JX3Box',
+        badge: {
+          text: '常用',
+          type: 'warning',
+        },
+        desc: '剑三真好玩，魔盒真好用',
+        link: 'https://www.jx3box.com/'
+      },
     ]
+
   },
   {
     title: '在线 AI 工具',
@@ -28,6 +39,16 @@ export const NAV_DATA: NavData[] = [
         icon: 'https://statics.moonshot.cn/kimi-chat/favicon.ico',
         title: 'Kimi',
         link: 'https://kimi.moonshot.cn/'
+      },
+      {
+        icon: 'https://cdn-bot.hunyuan.tencent.com/logo-v2.png',
+        title: '腾讯元宝',
+        link: 'https://yuanbao.tencent.com/'
+      },
+      {
+        icon: 'https://lf-flow-web-cdn.doubao.com/obj/flow-doubao/doubao/web/logo-icon.png',
+        title: '抖音豆包',
+        link: 'https://www.doubao.com/'
       },
     ]
   },
